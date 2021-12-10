3cdf3e5318423842adbeb13aef0618d46d573175
JaksonLeal <jleal338@unab.edu.co>
2021-12-10 18:48:26 -0500

    optimizacion
    archivo operaciones.js optimizado

--- src/main/resources/static/js/operaciones.js
+++ src/main/resources/static/js/operaciones.js
@@ -1,35 +1,21 @@
 $(document).ready(function() { 
-	//el codigo dentro se ejecutacuando toda la pagina este lista
-});
-
-async function sumar(){ //async function devuelve elemento promise
-	let datos = {};  // variable let solo puede ser usada en el metodo
-	
-	datos.num1 = document.getElementById('num1').value;
-	datos.num2 = document.getElementById('num2').value;
-	
-	const request = await fetch('sumar', { //await espera hasta que el promise este resuelto para continuar
-    	method: 'POST', //metodo post se usa para enviar datos al servidor 
-    	headers: {
-      	'Accept': 'application/json',
-      	'Content-Type': 'application/json'
-    		}, 
-		body: JSON.stringify(datos)
-    });
-    const respuesta = await request.json();
-        
-    document.querySelector('#respuesta').innerHTML = respuesta;
-    
- }
+	//el codigo dentro se ejecuta cuando toda la pagina este lista
+}); 
  
- async function restar(){
-	let datos = {};
+ async function operar(x){ //async function devuelve elemento promise
+	let datos = {}; // variable let solo puede ser usada en el metodo
 	
 	datos.num1 = document.getElementById('num1').value;
 	datos.num2 = document.getElementById('num2').value;
 	
-	const request = await fetch('restar', {
-    	method: 'POST',
+	let urlOpe = '';
+	if(x=='+'){urlOpe = 'sumar';}
+		else if(x=='-'){urlOpe = 'restar';}
+			else if(x=='*'){urlOpe = 'multiplicar';}
+				else if(x=='/'){urlOpe = 'dividir';}
+	
+	const request = await fetch(urlOpe, { //await espera hasta que el promise este resuelto para continuar
+    	method: 'POST', //metodo post se usa para enviar datos al servidor
     	headers: {
       	'Accept': 'application/json',
       	'Content-Type': 'application/json'
@@ -42,42 +28,3 @@ async function sumar(){ //async function devuelve elemento promise
     
  }
  
- async function multiplicar(){
-	let datos = {};
-	
-	datos.num1 = document.getElementById('num1').value;
-	datos.num2 = document.getElementById('num2').value;
-	
-	const request = await fetch('multiplicar', {
-    	method: 'POST',
-    	headers: {
-      	'Accept': 'application/json',
-      	'Content-Type': 'application/json'
-    		}, 
-		body: JSON.stringify(datos)
-    });
-    const respuesta = await request.json();
-    
-    document.querySelector('#respuesta').innerHTML = respuesta;
-    
- }
- 
- async function dividir(){
-	let datos = {};
-	
-	datos.num1 = document.getElementById('num1').value;
-	datos.num2 = document.getElementById('num2').value;
-	
-	const request = await fetch('dividir', {
-    	method: 'POST',
-    	headers: {
-      	'Accept': 'application/json',
-      	'Content-Type': 'application/json'
-    		}, 
-		body: JSON.stringify(datos)
-    });
-    const respuesta = await request.json();
-    
-    document.querySelector('#respuesta').innerHTML = respuesta;
-    
- }
